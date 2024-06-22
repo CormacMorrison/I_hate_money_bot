6eb67882475f6406ba747034599f1fce6f58039d
Cormac Morrison <z5477834@ad.unsw.edu.au>
2024-06-23 02:05:22 +1000

    apparetly this works better than polynomial regression and my neural network died so

--- teamName.py
+++ teamName.py
@@ -1,4 +1,3 @@
-
 import numpy as np
 
 ##### TODO #########################################
@@ -10,14 +9,27 @@ nInst = 50
 currentPos = np.zeros(nInst)
 
 
+## prcSoFar is a 2D array
+
 def getMyPosition(prcSoFar):
     global currentPos
+    # get dimension of the matrix (returns touple with rows and cols)
     (nins, nt) = prcSoFar.shape
     if (nt < 2):
         return np.zeros(nins)
+    # gets percentage change in natural log form much better for finance crap
     lastRet = np.log(prcSoFar[:, -1] / prcSoFar[:, -2])
+    makeSureDoingWell = np.log(prcSoFar[:, -1] / prcSoFar[:, -nt])
+    lastRet = lastRet + makeSureDoingWell
+
+    
+    # normalise
     lNorm = np.sqrt(lastRet.dot(lastRet))
     lastRet /= lNorm
+
+    # new position
     rpos = np.array([int(x) for x in 5000 * lastRet / prcSoFar[:, -1]])
+
+
     currentPos = np.array([int(x) for x in currentPos+rpos])
     return currentPos
